e67682be53cc487960e83d611c4a030010e6c369
MikaelCantini <mikaelac2.0@gmail.com>
2026-02-27 07:38:26 -0300

    Busca filmes lançados em 1997

--- Retorno Requisições.sql
+++ Retorno Requisições.sql
@@ -4,5 +4,8 @@ SELECT Nome,Ano FROM Filmes
 -- 2. Busca o nome e o ano em ordem Crescente pelo ano
 SELECT Nome,Ano FROM Filmes ORDER BY Ano ASC
 
--- 3.  Buscar pelo filme "de volta para o futuro", trazendo o nome, ano e a duração
+-- 3. Buscar pelo filme "de volta para o futuro", trazendo o nome, ano e a duração
 SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro'
+
+-- 4. Buscar os filmes lançados em 1997
+SELECT * FROM Filmes WHERE Ano = 1997

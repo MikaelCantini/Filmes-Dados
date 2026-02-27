dc7805286fb73c365c14f8712b3c39660b718bc0
MikaelCantini <mikaelac2.0@gmail.com>
2026-02-27 08:08:11 -0300

    Buscar ator do genero feminino em ordem alfabetica pelo primeiro nome

--- Retorno Requisições.sql
+++ Retorno Requisições.sql
@@ -21,3 +21,6 @@ SELECT Ano, COUNT(*) Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DES
 
 -- 8. Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
 SELECT * FROM Atores WHERE Genero = 'M'
+
+-- 9. Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT * FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome

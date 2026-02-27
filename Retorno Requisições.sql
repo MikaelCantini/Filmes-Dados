36896e7dbea323c3572311ef387eaed80a30c85a
MikaelCantini <mikaelac2.0@gmail.com>
2026-02-27 07:34:16 -0300

    Busca Nome,Ano,Duracao pelo Nome

--- Retorno Requisições.sql
+++ Retorno Requisições.sql
@@ -4,3 +4,5 @@ SELECT Nome,Ano FROM Filmes
 -- 2. Busca o nome e o ano em ordem Crescente pelo ano
 SELECT Nome,Ano FROM Filmes ORDER BY Ano ASC
 
+-- 3.  Buscar pelo filme "de volta para o futuro", trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro'
